db7fb2d9ad3eaff2415438a7e55dcfb8dd7cec2e
Garrett <Garrett@YUP>
2024-06-12 14:03:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment_6._2._2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 1, 2, 3, 4, };
+            int[] result = Product(nums);
+            
+            
+            
+        }
+
+        static int[] Product(int[] nums)
+        {
+            int n = nums.Length;
+
+            int[] leftSide = new int[n];
+
+            int[] rightSide = new int[n];
+
+            int[] result = new int[n];
+
+            int leftProduct = 1;
+            for (int i = 0; i < n; i++)
+            {
+                leftSide[i] = leftProduct;
+                leftProduct = leftProduct * nums[i];
+            }
+
+            int rightProduct = 1;
+            for(int i = 0;i < n; i--)
+            {
+                rightSide[i] = rightProduct;
+                rightProduct = rightProduct * nums[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = leftSide[i] * rightSide[i];
+            }
+            return result;
+
+        }
+        
+
+        
+    }
+}
